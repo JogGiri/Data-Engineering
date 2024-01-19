c1d9287134ce6e037c179a937635deac380a0481
Jog Giri <42950079+JogGiri@users.noreply.github.com>
2024-01-19 22:44:56 +0530

    Update Job_Notebook_Context.py

--- Job_Notebook_Context.py
+++ Job_Notebook_Context.py
@@ -3,3 +3,15 @@
 Context = dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson()
 
 TOKEN = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
+
+SHardURL = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
+base_url = SHardURL.split('//')[-1]
+run_env = base_url.split('.')[0]
+
+notebook_info = json.loads(dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson())
+  try:
+      #The tag jobId does not exists when the notebook is not triggered by dbutils.notebook.run(...) 
+      jobId = notebook_info["tags"]["jobId"] 
+  except: 
+      jobId = -1 
+  jobUrl = env_url + str(jobId) + "/run/1"    
